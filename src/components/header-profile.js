2624d5303e699c0f6748382f254f6f942cc9d756
Benjamin Hallihan <ben.hallihan1@gmail.com>
2023-05-07 23:46:00 -0300

    Implemented additional header interactivity

--- src/components/header-profile.js
+++ src/components/header-profile.js
@@ -2,15 +2,23 @@ import pfp from '../assets/pfp.jpg';
 import './header-profile.css';
 
 function HeaderProfile() {
-    return (
-        <header className="App-header">
-            <img src={pfp} className="Profile-photo" alt="Profile"/>
-            <div className="Profile-header">
-                <p className="Full-name">Benjamin Isaiah Hallihan</p>
-                <p className="Degree-text">B.A. Major Computer Science, Minor Commerce</p>
-            </div>
-        </header>
-    );
+  const handleClick = () => {
+    window.location.href = 'https://benjru.github.io/Portfolio/';
+  };
+
+  return (
+    <header className="App-header">
+      <div
+        className="Profile-photo"
+        style={{ backgroundImage: `url(${pfp})` }}
+        onClick={handleClick}
+      ></div>
+      <div className="Profile-header">
+        <p className="Full-name">Benjamin Isaiah Hallihan</p>
+        <p className="Degree-text">B.A. Major Computer Science, Minor Commerce</p>
+      </div>
+    </header>
+  );
 }
 
 export default HeaderProfile;
